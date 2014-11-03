c5b791ee065a53e21b4050671136986dfa2e38b8
Gerben Oolbekkink <g.j.w.oolbekkink@gmail.com>
2014-11-03 21:58:48 +0100

    Fix diffing

--- client/notify.py
+++ client/notify.py
@@ -6,24 +6,22 @@ from urllib.request import Request, urlopen
 
 def main():
     print('sending data to gource-live')
-    #os.chdir('../..')
     username = check_output(['git', 'config', '--get', 'user.name']).decode().replace('\n', '')
-    commithash = check_output(['git', 'log', '--format=format:%H', '-1']).decode()
-    changes = check_output(['git', 'diff', '--name-status', commithash]).decode()
-    changeslist = changes.splitlines();
+    commithash = check_output(['git', 'log', '--format=format:%H', '-2']).decode().splitlines()
+    changes = check_output(['git', 'diff', '--name-status', commithash[0], commithash[1]]).decode()
+    changeslist = changes.splitlines()
 
     out = ''
     for change in changeslist:
         data = change.split('\t')
         out += '%s|%s|%s|%s\n' % (int(time.time()), username, data[0], data[1])
 
-    print('change: %s' %out.encode())
 
     req = Request('http://localhost:3000', data=out.encode())
     req.add_header('X-Git-Event', 'commit')
 
     resp = urlopen(req)
-    print(resp.read())
+    print(resp.read().decode())
     
 if __name__=="__main__":
     main()
